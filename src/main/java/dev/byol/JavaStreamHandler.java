0f7399babb60633989613c03ff85e89a25979f22
Từ Phúc Nguyên <tuphucnguyen20051@gmail.com>
2026-05-15 15:12:28 +0700

    Add StreamLambdaHandler for AWS Lambda integration

---
+++ src/main/java/dev/byol/JavaStreamHandler.java
@@ -0,0 +1,32 @@
+package dev.byol.lambda;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import dev.byol.Application;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context)
+            throws IOException {
+        handler.proxyStream(input, output, context);
+    }
+}
